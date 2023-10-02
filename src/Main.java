12a178b5e9dd111ed800a79acdc9aaca7c84793e
jramosperez84 <jramosperez@alumnoifp.es>
2023-10-02 20:16:35 +0100

    Comentarios

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import controller.App;
 
 public class Main {
     public static void main(String[] args) {
-        App app = App.app();
-        app.applicationMenu();
+        App app = App.app(); // Named Constructor de la clase App
+        app.applicationMenu(); // Llamada a la función que ejecuta el programa.
     }
 }
--- src/controller/App.java
+++ src/controller/App.java
@@ -22,20 +22,20 @@ public class App {
         return new App();
     }
 
-    public void applicationMenu() {
+    public void applicationMenu() { // Función con el menú de la aplicación.
         Scanner dataEntry = new Scanner(System.in);
         String filePath, messageText, outputMessage;
         int menuOption;
-        UserInterface.mainMenu();
+        UserInterface.mainMenu(); // Imprime el menú de usuario en pantalla
         menuOption = Integer.parseInt(dataEntry.nextLine());
 
         switch (menuOption) {
             case Constants.ENCRYPT -> {
-                UserInterface.getDocumentName();
+                UserInterface.getDocumentName(); // Imprime petición de documento
                 filePath = dataEntry.nextLine();
-                UserInterface.getStrText();
+                UserInterface.getStrText(); // Imprime petición de texto a cifrar.
                 messageText = dataEntry.nextLine();
-                encryptMessage(filePath, messageText);
+                encryptMessage(filePath, messageText); // Llamada a la función que encripta el texto.
                 applicationMenu();
             }
             case Constants.DECRYPT -> {
@@ -51,20 +51,20 @@ public class App {
     }
 
     private void encryptMessage(String filePath, String messageText) {
-        Encrypt encrypt = Encrypt.encrypt();
-        fileManagement = FileManagement.fileManagement();
-        byte[] desKey = secretKey.getEncoded();
-        byte[] encryptedMessage = encrypt.encryptMessage(messageText, secretKey);
-        fileManagement.writeFile(filePath, encryptedMessage);
-        fileManagement.writeSecretKey(Constants.PREFIX.concat(filePath), desKey);
+        Encrypt encrypt = Encrypt.encrypt(); // Named Constructor de la clase Encrypt
+        fileManagement = FileManagement.fileManagement(); // Named Constructor de la clase FileManagement
+        byte[] desKey = secretKey.getEncoded(); // Guardamos la SecretKey en un byte[] para almacenarla en un documento.
+        byte[] encryptedMessage = encrypt.encryptMessage(messageText, secretKey); // Mensaje encriptado, mediante llamada a la función encryptMesaje de la clase Encrypt
+        fileManagement.writeFile(filePath, encryptedMessage); // Se cuarda el mensaje en el documento con la funcion writeFile
+        fileManagement.writeSecretKey(Constants.PREFIX.concat(filePath), desKey); // Guarda la clave en un documento con el mismo nombre de documento mas el prefijo key.
     }
 
     private String decryptMessage(String filePath) {
-        Decrypt decrypt = Decrypt.decrypt();
-        byte[] SecretKey = fileManagement.readFile(Constants.PREFIX.concat(filePath));
-        byte[] encryptedMessage = fileManagement.readFile(filePath);
-        SecretKey originalKey = new SecretKeySpec(SecretKey, Constants.ENCRYPT_SYSTEM);
-        return decrypt.decryptMessage(encryptedMessage, originalKey);
+        Decrypt decrypt = Decrypt.decrypt(); // Named Constructor de la clase Decrypt
+        byte[] SecretKey = fileManagement.readFile(Constants.PREFIX.concat(filePath)); // Recuperamos la clave mediante readFile
+        byte[] encryptedMessage = fileManagement.readFile(filePath); // Recuperamos la clave mediante readFile
+        SecretKey originalKey = new SecretKeySpec(SecretKey, Constants.ENCRYPT_SYSTEM); // Guardamos el array de byte recuperado del fichero key.documento.dat en una nueva instancia de SecretKey
+        return decrypt.decryptMessage(encryptedMessage, originalKey); // Devolvemos el mensaje desencriptado.
     }
 }
 
